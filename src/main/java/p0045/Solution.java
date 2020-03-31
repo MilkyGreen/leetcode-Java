5d05d770279c87e9a024d9119dfb87b74a840535
liyunmeng_ks <ymli@akazam.com>
2020-03-31 14:08:28 +0800

    45

---
+++ src/main/java/p0045/Solution.java
@@ -0,0 +1,30 @@
+package p0045;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+/**
+ * @author liyunmeng
+ * @Description:
+ * @Date 2020/3/31 11:17
+ */
+public class Solution {
+    public int jump(int[] nums) {
+
+        int ans = 0;
+        int max = 0;
+        int end = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            max = Math.max(max,nums[i] + i);
+            if(i == end){
+                end = max;
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        int[] nums={2,3,1,1,4};
+        System.out.println(new Solution().jump(nums));
+    }
+}
