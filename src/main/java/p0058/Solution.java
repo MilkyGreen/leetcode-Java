87113dec3290dd2759182dec7d2a9200fb2b699b
liyunmeng_ks <ymli@akazam.com>
2020-04-26 16:58:17 +0800

    58

---
+++ src/main/java/p0058/Solution.java
@@ -0,0 +1,34 @@
+package p0058;
+
+/**
+ * @author liyunmeng
+ * @Description: length-of-last-word
+ * easy
+ * 解题里没啥人用split，是我犯规了吗。。。
+ * @Date 2020/4/26 16:46
+ */
+public class Solution {
+
+    public int lengthOfLastWord(String s) {
+        if(s == null || "".equals(s)){
+            return 0;
+        }
+        String[] s1 = s.split(" ");
+        int l = s1.length;
+
+        for (int i = l -1; i >= 0; i--) {
+            String s2 = s1[i];
+            if(!"".equals(s2)){
+                return s2.length();
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLastWord("Hello World  "));
+    }
+
+}
