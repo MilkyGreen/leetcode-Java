6d060c1692ae0999480e5b0f410b6e9cbae1d272
李云梦.渠成易销 <yunmeng.li@successchannel.com>
2020-08-05 08:51:49 +0800

    82、83

--- src/main/java/p100/p0082/Solution.java
+++ src/main/java/p100/p0082/Solution.java
@@ -1,30 +1,67 @@
 package p100.p0082;
 
 /**
- * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * <p>
+ *     给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+ * //
+ * // 示例 1:
+ * //
+ * // 输入: 1->2->3->3->4->4->5
+ * //输出: 1->2->5
+ * //
+ * //
+ * // 示例 2:
+ * //
+ * // 输入: 1->1->1->2->3
+ * //输出: 2->3
  */
 public class Solution {
 
     /**
-     * 思路：遍历，遇到重复的跳过即可。
+     * 思路：遍历链表，记录最近一个不重复的节点preNonDupNode，不断更新preNonDupNode直到遇到重复节点，遍历完重复节点后，跳过中间重复的直接指向重复的next。
+     * 执行结果：速度 击败92% Java提交，噢耶。
      * @param head
      * @return
      */
     public ListNode deleteDuplicates(ListNode head) {
+
         if(head == null){
             return null;
         }
 
+        // 虚拟头节点
+        ListNode dump = new ListNode(0);
+        dump.next = head;
+        // 最近一个不重复节点
+        ListNode preNonDupNode = dump;
+
         ListNode currNode = head;
+        // 当前是否有重复节点未处理
+        boolean currHasDup = false;
         while (currNode.next != null){
-            ListNode next = currNode.next;
-            if(currNode.val == next.val){
-                currNode.next = next.next; // 注意跳过之后继续从currNode判断，后面还有可能重复
+
+            if(currNode.val != currNode.next.val){
+                // 两个节点不重复且前面有重复节点
+                if(currHasDup){
+                    currHasDup = false;
+                    // 上个不重复节点指向下个不重复节点，即把中间的重复节点跳过
+                    preNonDupNode.next = currNode.next;
+                }else {
+                    // 当前没有未处理重复节点，更新preNonDupNode即可
+                    preNonDupNode = currNode;
+                }
             }else {
-                currNode = next; // 继续下一个判断
+                currHasDup = true;
             }
+            currNode = currNode.next;
         }
-        return head;
+
+        // 如果列表最后有没处理的重复节点，则全部删掉
+        if(currHasDup){
+            preNonDupNode.next = null;
+        }
+
+        return dump.next;
     }
 
 }
---
+++ src/main/java/p100/p0083/Solution.java
@@ -0,0 +1,41 @@
+package p100.p0083;
+
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ */
+public class Solution {
+
+    /**
+     * 思路：遍历，遇到重复的跳过即可。
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return null;
+        }
+
+        ListNode currNode = head;
+        while (currNode.next != null){
+            ListNode next = currNode.next;
+            if(currNode.val == next.val){
+                currNode.next = next.next; // 注意跳过之后继续从currNode判断，后面还有可能重复
+            }else {
+                currNode = next; // 继续下一个判断
+            }
+        }
+        return head;
+    }
+
+}
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
