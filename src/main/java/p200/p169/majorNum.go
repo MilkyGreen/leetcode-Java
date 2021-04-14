736218d53b96ee85b06cfe8696d1ebe8c1ed8298
李云梦.渠成易销 <yunmeng.li@successchannel.com>
2021-04-14 11:55:12 +0800

    169

--- src/main/java/p200/p169/majorNum.go
+++ src/main/java/p200/p169/majorNum.go
@@ -21,3 +21,27 @@ func majorityElement(nums []int) int {
 	}
 	return nums[0]
 }
+
+/**
+思路：记录第一个数为major，数量为一。遍历数组，如果和major相同，count加一，不同count减一，count为0的时候更新major，最后的major就是结果。
+原理是一定存在major数，它一定能和其他所有的数抵消，还有剩余。
+**/
+func majorityElement2(nums []int) int {
+	majorNum := nums[0]
+	count := 1
+	for index,v := range nums{
+		if index >0 {
+			if v == majorNum{
+				count++
+			}else {
+				if count == 0{
+					count = 1
+					majorNum = v
+				}else {
+					count--
+				}
+			}
+		}
+	}
+	return majorNum
+}
