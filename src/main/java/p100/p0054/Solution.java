d051ac2063de12eca8fac69540a895dba8f322ae
李云梦.渠成易销 <yunmeng.li@successchannel.com>
2021-06-28 19:17:10 +0800

    54

--- src/main/java/p100/p0054/Solution.java
+++ src/main/java/p100/p0054/Solution.java
@@ -1,5 +1,6 @@
 package p100.p0054;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -9,8 +10,40 @@ import java.util.List;
  */
 public class Solution {
 
+    // 思路：记录每条边的位置，
     public List<Integer> spiralOrder(int[][] matrix) {
-        return null;
+        int top = 0; // 上面未遍历的位置
+        int left = 0; // 左边未遍历的位置
+        int bottom = matrix.length-1; // 底部未遍历的位置
+        int right = matrix[0].length-1; // 右边未遍历的位置
+        List<Integer> ans = new LinkedList<>();
+        int elm = matrix.length*matrix[0].length; // 总元素数量
+        while (elm >= 1){
+            for(int i=left;i<=right && elm >=1;i++){ // 先遍历上边
+                ans.add(matrix[top][i]);
+                elm--;
+            }
+            top++; // 上面下移
+
+            for(int i = top;i <= bottom && elm>=1;i++){
+                ans.add(matrix[i][right]);
+                elm--;
+            }
+            right--;
+
+            for(int i = right;i>=left&& elm>=1;i--){
+                ans.add(matrix[bottom][i]);
+                elm--;
+            }
+            bottom--;
+
+            for(int i = bottom;i>=top&& elm>=1;i--){
+                ans.add(matrix[i][left]);
+                elm--;
+            }
+            left++;
+        }
+        return ans;
     }
 
 }
