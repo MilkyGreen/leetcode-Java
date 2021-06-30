eb5f3cc8a50b57db4b2231c9a85c9e0e3bab8527
李云梦.渠成易销 <yunmeng.li@successchannel.com>
2021-06-30 23:16:40 +0800

    heapsort

---
+++ src/main/java/HeapSort.java
@@ -0,0 +1,65 @@
+/**
+ * <p>
+ * Copyright: Copyright (c) 2021/6/30 22:55
+ * <p>
+ * Company: 苏州渠成易销网络科技有限公司
+ * <p>
+ *
+ * @author yunmeng.li@successchannel.com
+ * @version 1.0.0
+ */
+public class HeapSort {
+
+    public void heapSort(int[] nums){
+        buildHeap(nums);
+        int l = nums.length;
+        for(int i = l-1;i>=0;i--){
+            swap(nums,0,i);
+            heapify(nums,0,i);
+        }
+    }
+
+    public void heapify(int[] nums,int n,int l){
+        if (n >= l){
+            return;
+        }
+        int c1 = n*2+1;
+        int c2 = n*2+2;
+        int max = n;
+        if(c1 < l && nums[max] < nums[c1]){
+            max = c1;
+        }
+
+        if(c2 < l && nums[max] < nums[c2]){
+            max = c2;
+        }
+        if(max != n){
+            swap(nums,max,n);
+            heapify(nums,max,l);
+        }
+    }
+
+    public void buildHeap(int[] nums){
+        int l = nums.length;
+        int lastNode = l -1; // 最后一个节点
+        int p = (lastNode -1) / 2; // 最后一个父节点
+        // 从最后一个父节点开始，依次向前heapify
+        for(int i=p;i>=0;i--){
+            heapify(nums,i,l);
+        }
+    }
+
+    private void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args){
+        int[] nums ={8,2,4,8,13,2,34,87,43,6};
+        new HeapSort().heapSort(nums);
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
