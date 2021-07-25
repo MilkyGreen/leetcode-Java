75209dcd68f5e74035fb0c5f5fe18cf7fdcbd8e8
李云梦.渠成易销 <yunmeng.li@successchannel.com>
2021-07-25 11:26:59 +0800

    周赛

--- src/main/java/weekcontest/N057/Solution3.java
+++ src/main/java/weekcontest/N057/Solution3.java
@@ -1,9 +1,6 @@
 package weekcontest.N057;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * <p>
@@ -12,40 +9,76 @@ public class Solution3 {
 
 
     public List<List<Long>> splitPainting(int[][] segments) {
-        HashMap<Integer, Long> map = new HashMap<>();
+        HashMap<Integer, List<Long>> map = new HashMap<>();
         int end = 0;
+        int start = segments[0][0]-1;
         for (int i = 0; i < segments.length; i++) {
             int[] segment = segments[i];
-            end = Math.max(end, segment[1] - 1);
-            for (int j = segment[0] - 1; j < segment[1] - 1; j++) {
-                map.put(j, map.getOrDefault(j, 0L) + segment[2]);
+            end = Math.max(end, segment[1] - 2);
+            start = Math.min(start, segment[0] - 1);
+            for (int j = segment[0] - 1; j <= segment[1] - 2; j++) {
+                List<Long> list = map.getOrDefault(j, new ArrayList<>());
+                list.add((long)segment[2]);
+                map.put(j,list);
             }
         }
+        map.values().forEach(list ->{
+            list.sort(Comparator.comparingLong(o -> o));
+        });
 
-        Long lastColor = 0L;
+        List<Long> lastColor = null;
         List<List<Long>> ans = new ArrayList<>();
-        Long s = 0L;
-        Long e = 0L;
-        for (int i = 0; i <= end; i++) {
-            Long aLong = map.get(i);
-            if (lastColor != 0L) {
-                if (lastColor != aLong) {
+        Long s = (long)start;
+        for (int i = start; i <= end+1; i++) {
+            List<Long> currList = map.get(i);
+            if (lastColor != null) {
+                if (currList == null || !sameColor(currList,lastColor)) {
                     ArrayList<Long> list = new ArrayList<>();
                     list.add(s + 1);
-                    list.add(e + 1);
-                    list.add(lastColor);
+                    list.add(i + 1L);
+                    list.add(sumList(lastColor));
                     ans.add(list);
-                    s = e;
+                    s = (long) i;
                 }
+            }else {
+                s = (long) i;
             }
-            e++;
-            lastColor = aLong;
+            lastColor = currList;
         }
-
         return ans;
     }
 
-    public static void main(String[] args) {
+    private boolean sameColor(List<Long> a,List<Long> b){
+        if(a.size() != b.size()){
+            return false;
+        }
+        for (int i = 0; i < a.size(); i++) {
+            if(a.get(i) != b.get(i)){
+                return false;
+            }
+        }
+        return true;
+    }
 
+    private Long sumList(List<Long> list){
+        Long sum = 0L;
+        for (Long aLong : list) {
+            sum += aLong;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] segments = new int[4][3];
+        int[] a = new int[]{2,4,5};
+        int[] b = new int[]{2,4,7};
+        int[] c = new int[]{4,7,1};
+        int[] d = new int[]{4,7,11};
+        segments[0] = a;
+        segments[1] = b;
+        segments[2] = c;
+        segments[3] = d;
+
+        new Solution3().splitPainting(segments);
     }
 }
