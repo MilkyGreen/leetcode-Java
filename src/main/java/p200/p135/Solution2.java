3d5d85479cc6d5f89fb953be5fbb5918cb8bfe58
李云梦 <yunmeng.li@successchannel.com>
2022-04-02 10:01:05 +0800

    139

---
+++ src/main/java/p200/p135/Solution2.java
@@ -0,0 +1,24 @@
+package p200.p135;
+
+public class Solution2 {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int ret = 0;
+        int[] cs = new int[n];
+        Arrays.fill(cs,1);
+        for(int i = 1;i<n;i++){
+            if(ratings[i] > ratings[i-1]){
+                cs[i] = cs[i-1]+1;
+            }
+        }
+        for(int i = n-2;i>=0;i--){
+            if(ratings[i] > ratings[i+1] && cs[i] <= cs[i+1]){
+                cs[i] = cs[i+1] + 1;
+            }
+        }
+        for(Integer c : cs){
+            ret += c;
+        }
+        return ret;
+    }
+}
