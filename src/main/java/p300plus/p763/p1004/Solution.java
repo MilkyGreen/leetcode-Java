e081fc9521027e6df10edc33495db8fbfc6e815c
李云梦 <yunmeng.li@successchannel.com>
2022-05-22 10:23:07 +0800

    1004

---
+++ src/main/java/p300plus/p763/p1004/Solution.java
@@ -0,0 +1,25 @@
+package p300plus.p763.p1004;
+
+public class Solution {
+    public int longestOnes(int[] nums, int k) {
+
+        int ret = 0;
+        int left = 0;
+        int right = 0;
+        int zeroNum = 0;
+        while(right < nums.length && left <= right){
+            if(nums[right] == 0){
+                zeroNum++;
+            }
+            while(zeroNum > k){
+                if(nums[left] == 0){
+                    zeroNum--;
+                }
+                left++;
+            }
+            ret = Math.max(ret,right - left + 1);
+            right++;
+        }
+        return ret;
+    }
+}
