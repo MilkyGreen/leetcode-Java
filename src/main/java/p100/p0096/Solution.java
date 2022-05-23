c373b7153cc2d8eed9e6424fcdabdfa8a9ce2863
李云梦 <yunmeng.li@successchannel.com>
2022-05-23 09:16:32 +0800

    96

--- src/main/java/p100/p0096/Solution.java
+++ src/main/java/p100/p0096/Solution.java
@@ -6,10 +6,29 @@ package p100.p0096;
  */
 public class Solution {
 
+    Map<Integer,Integer> map = new HashMap<>();
     public int numTrees(int n) {
-
-
-        return 0;
+        if(map.containsKey(n)){
+            return map.get(n);
+        }
+        if(n == 0){
+            map.put(0,1);
+            return 1;
+        }
+        if(n == 1 || n == 2){
+            map.put(1,1);
+            map.put(2,2);
+            return n;
+        }
+        int ret = 0;
+        for(int i = 0;i < n;i++){
+            int left = numTrees(i);
+            int right = numTrees(n-i-1);
+            int num = left * right;
+            ret += num;
+        }
+        map.put(n, ret);
+        return ret;
     }
 
 }
