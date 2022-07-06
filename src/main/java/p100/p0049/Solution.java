8a4873d22236422bfc6480e9b3404552d50b9ed1
李云梦 <yunmeng.li@successchannel.com>
2022-07-06 09:33:23 +0800

    49

--- src/main/java/p100/p0049/Solution.java
+++ src/main/java/p100/p0049/Solution.java
@@ -12,21 +12,72 @@ import java.util.List;
  */
 public class Solution {
 
+    Map<String,Map<Character,Integer>> strCharCount = new HashMap<>();
+
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String str : strs) {
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            List<String> list = map.computeIfAbsent(new String(chars), k -> new ArrayList<>());
-            list.add(str);
+        List<List<String>> ret = new ArrayList<>();
+        // str -> anagrams
+        Map<String,List<String>> groups = new HashMap<>();
+        Map<Integer,List<String>> lenStrs = new HashMap<>();
+
+        for(String str : strs){
+            int len = str.length();
+            boolean has = false;
+            if(lenStrs.containsKey(len)){
+                List<String> sameLens = lenStrs.get(len);
+                for(String s : sameLens){
+                    if(isAnagrams(s,str)){
+                        has = true;
+                        groups.get(s).add(str);
+                    }
+                }
+                if(!has){
+                    sameLens.add(str);
+                }
+            }else{
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                lenStrs.put(len,list);
+            }
+            if(!has){
+                List<String> strGroups = new ArrayList<>();
+                strGroups.add(str);
+                groups.put(str,strGroups);
+
+
+            }
         }
-        return new ArrayList<>(map.values());
+
+        for(Map.Entry<String,List<String>> entry : groups.entrySet()){
+            ret.add(entry.getValue());
+        }
+
+        return ret;
     }
 
-    public static void main(String[] args){
-        Solution solution = new Solution();
-        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        solution.groupAnagrams(strs);
+    boolean isAnagrams(String a,String b){
+        Map<Character,Integer> aMap = countChar(a);
+        Map<Character,Integer> bMap = countChar(b);
+
+        for(Map.Entry<Character,Integer> entry : aMap.entrySet()){ 
+            if(!(bMap.containsKey(entry.getKey()) && bMap.get(entry.getKey()) == entry.getValue())){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    Map<Character,Integer> countChar(String str){
+        if(strCharCount.containsKey(str)){
+            return strCharCount.get(str);
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i = 0;i < str.length();i++){
+            char c = str.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        strCharCount.put(str,map);
+        return map;
     }
 
 }
