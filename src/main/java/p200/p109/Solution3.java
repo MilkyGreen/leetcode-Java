2611218b66f324d962510294a813e862c7e477ec
李云梦 <yunmeng.li@successchannel.com>
2022-09-07 08:54:24 +0800

    109

---
+++ src/main/java/p200/p109/Solution3.java
@@ -0,0 +1,27 @@
+package p200.p109;
+
+public class Solution3 {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return new TreeNode(head.val);
+        }
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null;
+
+        TreeNode root = new TreeNode(mid.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(mid.next);
+        return root;
+    }
+}
