4556e7636673e5a66eca3ade9fd1047da55d272b
李云梦 <yunmeng.li@successchannel.com>
2022-10-10 09:34:38 +0800

    331

--- src/main/java/p300plus/p331/Solution.java
+++ src/main/java/p300plus/p331/Solution.java
@@ -1,26 +1,33 @@
 package p300plus.p331;
 
 public class Solution {
+    // 遍历到的位置
     int idx = 0;
-    String[] cs;
+    String[] nodes;
     public boolean isValidSerialization(String preorder) {
-        this.cs = preorder.split(",");
+        this.nodes = preorder.split(",");
         if(isValid()){
-            return idx == cs.length;
+            // 从根开始是合法二叉树，看看后面还有没有元素了，还有说明是多余的
+            return idx == nodes.length;
         }else{
             return false;
         }
     }
 
+    // 验证从idx位置开始往后是不是合法二叉树
     public boolean isValid(){
-        if(idx >= cs.length){
+        // 索引越界了
+        if(idx >= nodes.length){
             return false;
         }
-        if(cs[idx].equals("#")){
-            idx++;
+        // 如果是#，就算是一个空树
+        if(nodes[idx].equals("#")){
+            idx++; // 注意索引要加一
             return true;
         }
+        // 不是#，把当前字符跳过
         idx++;
+        // 分别验证左右子树是否合法
         return isValid() && isValid();
     }
 }
